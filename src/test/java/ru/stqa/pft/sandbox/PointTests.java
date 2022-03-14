0b4e4a696ff738b9cfb4667c18a0ad977f065961
sboyko <your_email@whatever.comm909mk@gmail.comgit config --global user.email your_email@whatever.com>
2022-03-14 19:21:08 +0300

    Задание №3 - доработка

--- src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -10,28 +10,28 @@ public class PointTests {
     public void testDistanceFirstPointMoreThanSecond() {
         Point p1 = new Point(11.0, 10.0);
         Point p2 = new Point(3.0, 6.0);
-        Assert.assertEquals(p1.distance(p1,p2),6.928203230275509);
+        Assert.assertEquals(p1.distance(p2),6.928203230275509);
     }
 
     @Test
     public void testDistanceBothPointsAtSamePosition() {
         Point p1 = new Point(11.0, 10.0);
         Point p2 = new Point(11.0, 10.0);
-        Assert.assertEquals(p1.distance(p1,p2),0);
+        Assert.assertEquals(p1.distance(p2),0);
     }
 
     @Test
     public void testDistanceSecondPointMoreThanFirst() {
         Point p1 = new Point(10.0, 9.0);
         Point p2 = new Point(11.0, 10.0);
-        Assert.assertEquals(p1.distance(p1,p2),0.0);
+        Assert.assertEquals(p1.distance(p2),0.0);
     }
 
     @Test
     public void testDistanceMinusCoordinates() {
         Point p1 = new Point(-10.0, -9.0);
         Point p2 = new Point(-11.0, -10.0);
-        Assert.assertEquals(p1.distance(p1,p2),0.0);
+        Assert.assertEquals(p1.distance(p2),0.0);
     }
 
 
