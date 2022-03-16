ed4525942fb2a57065437dc805830c1b24ba5796
sboyko <your_email@whatever.comm909mk@gmail.comgit config --global user.email your_email@whatever.com>
2022-03-16 10:08:22 +0300

    Задание 2,3 - исправление формулы расстония между двумя точками в двумерном пространстве и тестов к методу. Хотелось как можно лаконичнее написать формулу, используя методы класса Math. Извините)

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,8 @@ import ru.stqa.pft.sandbox.Point;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        Point p1 = new Point(11.0, 10.0);
-        Point p2 = new Point(3.0, 6.0);
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
 
         //System.out.println("Расстояние между двумя точками равно " + p1.distance(p1, p2));
         System.out.println("Расстояние между двумя точками равно " + p1.distance(p2));
--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -10,8 +10,6 @@ public class Point {
     }
 
     public double distance(Point p2) {
-        double x = (p2.x - this.x);
-        double y = (p2.y - this.y);
-        return Math.sqrt(Math.pow(x, 2) - Math.pow(y, 2));
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
 }
