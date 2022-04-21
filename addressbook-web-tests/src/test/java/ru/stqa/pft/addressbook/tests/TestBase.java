8f1fe8d37ac5064860a0b8d390ed0efd4c1a8815
sboyko <m909mk@gmail.com>
2022-04-21 20:13:00 +0300

    6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import static org.openqa.selenium.remote.BrowserType.CHROME;
 
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", CHROME));
@@ -18,9 +27,19 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m, Object[] p){
+        logger.info("Stop test " + m.getName() + "with parameters " + Arrays.asList(p));
     }
 
     public void createGroupIfItNotExists(GroupData groupData) {
