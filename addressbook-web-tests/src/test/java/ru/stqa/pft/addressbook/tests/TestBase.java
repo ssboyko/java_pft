9fc250ee9d4839e2c2bc8664d83955cd482a5c27
sboyko <m909mk@gmail.com>
2022-04-21 19:00:15 +0300

    6.10. Создание properties. Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import static org.openqa.selenium.remote.BrowserType.CHROME;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
